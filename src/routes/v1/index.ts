a10ce5d4e9f9c9bee2c09a0b51b56e9e42801a3e
UdayaKiran442 <72691871+UdayaKiran442@users.noreply.github.com>
2025-03-29 11:35:35 +0530

    TASK: api for adding waitlist users

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,16 +1,17 @@
 /*
  * v1 API Routes
- * 
+ *
  * This module handles all v1 API routes.
  */
 
 import { Hono } from "hono";
 
-import authRouter from './auth/auth.router';
-import datasetsRoute from './datasets/datasets.route';
-import rowsRoute from './rows/rows.route';
-import columnsRoute from './columns/columns.route';
-import testRoute from '../../routes/test/test.route';
+import authRouter from "./auth/auth.router";
+import datasetsRoute from "./datasets/datasets.route";
+import rowsRoute from "./rows/rows.route";
+import columnsRoute from "./columns/columns.route";
+import testRoute from "../../routes/test/test.route";
+import waitListRoute from "./waitlist/waitist.route";
 
 const v1Router = new Hono();
 
@@ -19,6 +20,7 @@ v1Router.route("/auth", authRouter);
 v1Router.route("/datasets", datasetsRoute);
 v1Router.route("/rows", rowsRoute);
 v1Router.route("/columns", columnsRoute);
+v1Router.route("/waitlist", waitListRoute);
 v1Router.route("/test", testRoute);
 
 export default v1Router;
