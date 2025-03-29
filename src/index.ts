d851c9b0f53e4232dfeb5ee36d7cd4be17b2d845
UdayaKiran442 <72691871+UdayaKiran442@users.noreply.github.com>
2025-03-29 12:00:35 +0530

    FIX: add cors origin

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,37 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 import { prettyJSON } from "hono/pretty-json";
-import { cors } from 'hono/cors'
+import { cors } from "hono/cors";
 
-import { ActiveConfig } from './utils/config.utils'
-import apiRouter from './routes'
-import testRouter from './routes/test/test.route';
+import { ActiveConfig } from "./utils/config.utils";
+import apiRouter from "./routes";
+import testRouter from "./routes/test/test.route";
 
-const app = new Hono()
+const app = new Hono();
 
-const env = ActiveConfig.ENV
+const env = ActiveConfig.ENV;
 
-app.use("/*", cors({
-  origin: ["http://localhost:5173"]
-}))
-app.use(prettyJSON())
+app.use(
+  "/*",
+  cors({
+    origin: ["http://localhost:5173", "https://tweakai-waitlist.vercel.app/"],
+  })
+);
+app.use(prettyJSON());
 
 // Mount the API router which handles versioning
-app.route("/api", apiRouter)
+app.route("/api", apiRouter);
 
 // Mount test routes
 app.route("/test", testRouter);
 
-app.get('/', (c) => {
-  return c.text(`Hello Hono! Welcome to TweakAI. Running in ${env} environment`)
-})
+app.get("/", (c) => {
+  return c.text(
+    `Hello Hono! Welcome to TweakAI. Running in ${env} environment`
+  );
+});
 
-app.get('/health-check', (c) => {
-  return c.text(`Server is healthy and running in ${env} environment`)
-})
+app.get("/health-check", (c) => {
+  return c.text(`Server is healthy and running in ${env} environment`);
+});
 
-export default app
+export default app;
